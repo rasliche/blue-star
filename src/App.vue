5ada44855b9d12a598fdfb4f421368629c5e8eda
EricR <rasliche@gmail.com>
2017-12-13 20:29:34 -0500

    g

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,38 @@
 <template>
-  <div id="app">
-    <user v-for="(user, index) in users" :name="user.name" :role="user.role"></user>
-  </div>
+    <div>
+        <home></home>
+    </div>
+    <!-- <div id="app">
+        <admin :shops="shops" :users="users"></admin>
+    </div> -->
 </template>
 
 <script>
-import User from './User.vue';
+import Admin from './components/Admin.vue';
+import Home from './components/Home.vue';
 
 export default {
-  components: {
-    user: User
-  },
-  name: 'app',
-  data () {
-    return {
-      users: [
-        {
-          name: "Eric",
-          role: "Admin"
-        },
-        {
-          name: "Joe",
-          role: "Manager",
-          shop: "Dive Keys"
-        },
-        {
-          name: "Madeline",
-          role: "Shop Staff",
-          shop: "Dive Keys"
-        },
-        {
-          name: "Christine",
-          role: "Shop Staff",
-          shop: "Scuba Shack"
-        },
-        {
-          name: "Oscar",
-          role: "Shop Staff",
-          shop: "Dive Keys"
-        },
-        {
-          name: "Big Bird",
-          role: "Shop Staff",
-          shop: "Underwater Adventures"
+    components: {
+        "home": Home,
+        "admin": Admin
+    },
+    name: 'app',
+    data () {
+        return {
+            shops: ['FKNMS', 'Rainbow Reef', 'Horizon Divers', 'Dive Key West', 'Tortuga Sailing Adventures'],
+            users: ['Chris Christopherson', 'Adam Stevens', 'Bob Barker', 'Steve Irwin', 'Bob Vila', 'Grant Imahara', 'Master Chief', 'Cortana Windows', 'The Colonel']
+          }
         }
-      ]
     }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+<div class="container mx-auto font-sans">
+    <header>
+        <h1>FKNMS Education Headquarters</h1>
+        <h2>Inform. Guide. Explore.</h2>
+    </header>
+    <div class="tracks flex justify-center py-8">
+        <div class="blue-star text-center flex-1">
+            <h3 class="border-blue-light border-2 rounded-lg">Blue Star</h3>
+        </div>
+        <div class="naturalist text-center flex-1">
+            <h3>Naturalist</h3>
+        </div>
+        <div class="team-ocean text-center flex-1">
+            <h3>Team OCEAN</h3>
+        </div>
+        <div class="marine-debris text-center flex-1">
+            <h3>Marine Debris Cleanup</h3>
+        </div>
+    </div>
+    <div class="modules flex flex-wrap">
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">101</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Rules/Regs</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Interpretation</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Marine Debris</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Seagrass I</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Corals I</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Mangroves I</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Maritime Heritage Resources I</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Diver Management</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Seagrass II</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Corals II</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Mangroves II</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Maritime Heritage Resources II</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Current Science I</p>
+        <p class="w-1/5 text-center py-2">Seagrass III</p>
+        <p class="w-1/5 text-center py-2">Corals III</p>
+        <p class="w-1/5 text-center py-2">Mangroves III</p>
+        <p class="w-1/5 text-center py-2">Maritime Heritage Resources III</p>
+        <p class="w-1/5 text-center py-2">Current Science II</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Zones</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Marine Life</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Reporting/Response</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Lionfish</p>
+        <p class="w-1/5 text-center py-2">Current Science III</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Buoys</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Hazardous Marine Life</p>
+        <p class="w-1/5 text-center py-2">Vessels</p>
+        <p class="w-1/5 text-center py-2 border-blue-light border-2 rounded-lg">Lionfish II</p>
+        <p class="w-1/5 text-center py-2">Safety</p>
+        <p class="w-1/5 text-center py-2">Volunteer Requirements</p>
+        <p class="w-1/5 text-center py-2"></p>
+        <p class="w-1/5 text-center py-2"></p>
+        <p class="w-1/5 text-center py-2"></p>
+        <p class="w-1/5 text-center py-2"></p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mods: ['101', 'Rules/Regs', 'Interpretation', 'Marine Debris', 'Seagrass 1', 'Seagrass 2', 'Seagrass 3']
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
